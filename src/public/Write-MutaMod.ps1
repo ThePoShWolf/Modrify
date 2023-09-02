b34fbee996449ddaa7d69c7c063aa94992cd7af1
Anthony Howell <anthony@howell-it.com>
2023-09-02 06:37:43 -0700

    added compression fix

--- src/public/Write-MutaMod.ps1
+++ src/public/Write-MutaMod.ps1
@@ -5,10 +5,14 @@ Function Write-MutaMod {
         [System.IO.FileInfo]$Path,
         [Mutagen.Bethesda.Plugins.Binary.Parameters.BinaryWriteParameters]$BinaryWriteParameters = $null,
         [Mutagen.Bethesda.Plugins.Binary.Parameters.ParallelWriteParameters]$ParallelWriteParameters = $null,
-        [System.IO.Abstractions.IFileSystem]$FileSystem = $null
+        [System.IO.Abstractions.IFileSystem]$FileSystem = $null,
+        [switch]$SkipCompressionFix
     )
     if ($Path.Name -ne $Mod.ModKey.FileName -and @('CorrectToPath', 'ThrowIfMisaligned') -contains $BinaryWriteParameters.ModKey) {
         Throw "Output name must match modkey. Override with `$BinaryWriteParameters.Modkey"
+    }
+    if (-not $SkipCompressionFix.IsPresent) {
+        Get-MutaModMajorRecords -Mod $Mod | ForEach-Object { $_.IsCompressed = $false }
     }
     [Mutagen.Bethesda.Skyrim.SkyrimModMixIn]::WriteToBinaryParallel($mod, $Path, $BinaryWriteParameters, $ParallelWriteParameters, $FileSystem)
 }

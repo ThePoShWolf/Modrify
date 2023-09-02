ee312cd8b029f90c718b4171b0dba67b0a3ac094
Anthony Howell <anthony@howell-it.com>
2023-09-02 06:29:24 -0700

    added check for modkey

--- src/public/Write-MutaMod.ps1
+++ src/public/Write-MutaMod.ps1
@@ -7,5 +7,8 @@ Function Write-MutaMod {
         [Mutagen.Bethesda.Plugins.Binary.Parameters.ParallelWriteParameters]$ParallelWriteParameters = $null,
         [System.IO.Abstractions.IFileSystem]$FileSystem = $null
     )
+    if ($Path.Name -ne $Mod.ModKey.FileName -and @('CorrectToPath', 'ThrowIfMisaligned') -contains $BinaryWriteParameters.ModKey) {
+        Throw "Output name must match modkey. Override with `$BinaryWriteParameters.Modkey"
+    }
     [Mutagen.Bethesda.Skyrim.SkyrimModMixIn]::WriteToBinaryParallel($mod, $Path, $BinaryWriteParameters, $ParallelWriteParameters, $FileSystem)
 }

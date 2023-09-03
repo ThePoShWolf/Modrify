ecef0e81f4a8a5dc4ef04cc8a33810f43ac67e7e
Anthony Howell <anthony@howell-it.com>
2023-09-03 15:23:53 -0700

    adding fallout 4 formats

--- formatting/Mods.format.ps1
+++ formatting/Mods.format.ps1
@@ -1,4 +1,9 @@
 $properties = @('ModKey', 'GameRelease')
+
+# Skyrim
 Write-FormatView -TypeName Mutagen.Bethesda.Skyrim.SkyrimMod -Property $properties -AutoSize
-Write-FormatView -TypeName Mutagen.Bethesda.Skyrim.ISkyrimModDisposableGetter -Property $properties -AutoSize
 Write-FormatView -TypeName Mutagen.Bethesda.Skyrim.SkyrimModBinaryOverlay -Property $properties -AutoSize
+
+# Fallout 4
+Write-FormatView -TypeName Mutagen.Bethesda.Fallout4.Fallout4Mod -Property $properties -AutoSize
+Write-FormatView -TypeName Mutagen.Bethesda.Fallout4.Fallout4ModBinaryOverlay -Property $properties -AutoSize

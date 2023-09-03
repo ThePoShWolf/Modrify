1ed3179385f7d2dce4eacc04a907f6ff5ebf865c
Anthony Howell <anthony@howell-it.com>
2023-09-03 14:57:28 -0700

    added binary overlay

--- formatting/Mods.format.ps1
+++ formatting/Mods.format.ps1
@@ -1,3 +1,4 @@
 $properties = @('ModKey', 'GameRelease')
 Write-FormatView -TypeName Mutagen.Bethesda.Skyrim.SkyrimMod -Property $properties -AutoSize
 Write-FormatView -TypeName Mutagen.Bethesda.Skyrim.ISkyrimModDisposableGetter -Property $properties -AutoSize
+Write-FormatView -TypeName Mutagen.Bethesda.Skyrim.SkyrimModBinaryOverlay -Property $properties -AutoSize

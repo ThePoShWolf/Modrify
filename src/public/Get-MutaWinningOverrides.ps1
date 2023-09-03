25c8e65ebeaf46438dc648fbd755a43bae92f0fa
Anthony Howell <anthony@howell-it.com>
2023-09-03 14:57:42 -0700

    moved type completer to onload

--- src/public/Get-MutaWinningOverrides.ps1
+++ src/public/Get-MutaWinningOverrides.ps1
@@ -22,34 +22,3 @@ Function Get-MutaWinningOverrides {
     Write-Verbose "Type: $typeName"
     $([Mutagen.Bethesda.OverrideMixIns]::WinningOverrides($MutagenGameEnvironment.LoadOrder.PriorityOrder, $typeName , $IncludeDeletedRecords))
 }
-
-$sb = {
-    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
-    if (-not (Get-Variable 'MajorRecordTypeNameCompleters' -Scope Script)) {
-        # loading all types in Mutagen.Bethesda*
-        $allTypes = [AppDomain]::CurrentDomain.GetAssemblies().GetTypes() | Where-Object { $_.Namespace -like 'Mutagen.Bethesda*' }
-        # filtering for types that implement the ISkyrimMajorRecordGetter
-        # this will need to be Improved to account for other games
-        $getterSearch = Switch -Regex ($MutagenGameEnvironment.GameRelease) {
-            '^Skyrim' {
-                'Mutagen.Bethesda.Skyrim.ISkyrimMajorRecordGetter'
-            }
-            '^Fallout' {
-                'Mutagen.Bethesda.Fallout4.IFallout4MajorRecordGetter'
-            }
-            default {
-                'Mutagen.Bethesda.Skyrim.ISkyrimMajorRecordGetter'
-            }
-        }
-        $mrgs = $allTypes | Where-Object { $_.ImplementedInterfaces.FullName -contains $getterSearch }
-        # convert to short type name
-        # i.e. INpcGetter to Npc
-        $script:MajorRecordTypeNameCompleters = $mrgs | Where-Object { $_.name -like '*Getter' } | ForEach-Object {
-            $_.Name -replace '^I(.*)Getter', '$1'
-        }
-    }
-    $MajorRecordTypeNameCompleters | Where-Object { $_ -like "$wordToComplete*" } | ForEach-Object {
-        $_
-    }
-}
-Register-ArgumentCompleter -CommandName Get-MutaWinningOverrides -ParameterName Type -ScriptBlock $sb

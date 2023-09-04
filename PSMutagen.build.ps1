a8d437ec22124c1573a07c6ad3e115e6c48f7035
Anthony Howell <anthony@howell-it.com>
2023-09-04 06:33:06 -0700

    build is more module, added quickbuild

--- PSMutagen.build.ps1
+++ PSMutagen.build.ps1
@@ -49,7 +49,7 @@ task DocBuild ModuleBuild, {
     }
 }
 
-task DownloadDependencies {
+task UpdateDependencies {
     foreach ($m in $modules.Keys) {
         if ((-not [string]::IsNullOrEmpty($module)) -and $m -ne $module) {
             continue
@@ -58,43 +58,6 @@ task DownloadDependencies {
         dotnet restore
         dotnet build
         Set-Location $PSScriptRoot
-    }
-}
-
-# Build the module
-task ModuleBuild Clean, DownloadDependencies, {
-    foreach ($m in $modules.Keys) {
-        if ((-not [string]::IsNullOrEmpty($module)) -and $m -ne $module) {
-            continue
-        }
-        $moduleScriptFiles = Get-ChildItem $modules[$m].srcPath -Filter *.ps1 -File -Recurse
-        if (-not(Test-Path $modules[$m].modulePath)) {
-            New-Item $modules[$m].modulePath -ItemType Directory
-        }
-
-        # Add using.ps1 to the .psm1 first
-        foreach ($file in $moduleScriptFiles | Where-Object { $_.Name -eq 'using.ps1' }) {
-            if ($file.fullname) {
-                Write-Host "Adding using file: '$($file.Fullname)'"
-                Get-Content $file.fullname | Out-File "$($modules[$m].modulePath)\$($modules[$m].moduleName).psm1" -Append -Encoding utf8
-            }
-        }
-
-        # Add all .ps1 files to the .psm1, skipping onload.ps1, using.ps1, and any tests
-        foreach ($file in $moduleScriptFiles | Where-Object { $_.Name -ne 'onload.ps1' -and $_.Name -ne 'using.ps1' -and $_.FullName -notmatch '(\\|\/)tests(\\|\/)[^\.]+\.tests\.ps1$' }) {
-            if ($file.fullname) {
-                Write-Host "Adding function file: '$($file.FullName)'"
-                Get-Content $file.fullname | Out-File "$($modules[$m].modulePath)\$($modules[$m].moduleName).psm1" -Append -Encoding utf8
-            }
-        }
-    
-        # Add the onload.ps1 files last
-        foreach ($file in $moduleScriptFiles | Where-Object { $_.Name -eq 'onload.ps1' }) {
-            if ($file.fullname) {
-                Write-Host "Adding onload file: '$($file.FullName)'"
-                Get-Content $file.fullname | Out-File "$($modules[$m].modulePath)\$($modules[$m].moduleName).psm1" -Append -Encoding utf8
-            }
-        }
 
         # Copy the dependency .dlls
         if (-not (Test-Path "$($modules[$m].modulePath)\lib" -PathType Container)) {
@@ -106,18 +69,72 @@ task ModuleBuild Clean, DownloadDependencies, {
         Get-ChildItem "$($modules[$m].srcPath)\bin\Debug\*.dll" | Where-Object { $_.Name -notlike "$($modules[$m].moduleName).dependencies*" -and $filesToSkip.Name -notcontains $_.Name } | ForEach-Object {
             Move-Item $_.FullName -Destination "$($modules[$m].modulePath)\lib\" -Force
         }
+    }
+}
 
-        # Copy the manifest
-        Copy-Item "$($modules[$m].srcPath)\$($modules[$m].moduleName).psd1" -Destination $modules[$m].modulePath
+task BuildModuleFile {
+    foreach ($m in $modules.Keys) {
+        if ((-not [string]::IsNullOrEmpty($module)) -and $m -ne $module) {
+            continue
+        }
+        $moduleScriptFiles = Get-ChildItem $modules[$m].srcPath -Filter *.ps1 -File -Recurse
+        $psm1Path = "$($modules[$m].modulePath)\$($modules[$m].moduleName).psm1"
+        if (-not(Test-Path $modules[$m].modulePath)) {
+            New-Item $modules[$m].modulePath -ItemType Directory
+        }
 
-        # Generate the formats
-        & "$($modules[$m].basePath)\$($modules[$m].moduleName).ezout.ps1" -RelativeDestination "build/$($modules[$m].moduleName)"
+        # Add using.ps1 to the .psm1 first
+        foreach ($file in $moduleScriptFiles | Where-Object { $_.Name -eq 'using.ps1' }) {
+            if ($file.fullname) {
+                Write-Host "Adding using file: '$($file.Fullname)'"
+                Get-Content $file.fullname | Out-File $psm1Path -Append -Encoding utf8
+            }
+        }
+
+        # Add all .ps1 files to the .psm1, skipping onload.ps1, using.ps1, and any tests
+        foreach ($file in $moduleScriptFiles | Where-Object { $_.Name -ne 'onload.ps1' -and $_.Name -ne 'using.ps1' -and $_.FullName -notmatch '(\\|\/)tests(\\|\/)[^\.]+\.tests\.ps1$' }) {
+            if ($file.fullname) {
+                Write-Host "Adding function file: '$($file.FullName)'"
+                Get-Content $file.fullname | Out-File $psm1Path -Append -Encoding utf8
+            }
+        }
+    
+        # Add the onload.ps1 files last
+        foreach ($file in $moduleScriptFiles | Where-Object { $_.Name -eq 'onload.ps1' }) {
+            if ($file.fullname) {
+                Write-Host "Adding onload file: '$($file.FullName)'"
+                Get-Content $file.fullname | Out-File $psm1Path -Append -Encoding utf8
+            }
+        }
 
         # Copy the tests
         foreach ($test in ($moduleScriptFiles | Where-Object { $_.FullName -match '(\\|\/)tests(\\|\/)[^\.]+\.tests\.ps1$' })) {
             Write-Host "Copying test file: '$($test.FullName)'"
             Copy-Item $test.FullName -Destination $modules[$m].modulePath
         }
+    }
+}
+
+task GenerateFormats {
+    foreach ($m in $modules.Keys) {
+        if ((-not [string]::IsNullOrEmpty($module)) -and $m -ne $module) {
+            continue
+        }
+        # Generate the formats
+        & "$($modules[$m].basePath)\$($modules[$m].moduleName).ezout.ps1" -RelativeDestination "build/$($modules[$m].moduleName)"
+    }
+}
+
+# Build the module
+task ModuleBuild Clean, UpdateDependencies, BuildModuleFile, GenerateFormats, {
+    foreach ($m in $modules.Keys) {
+        if ((-not [string]::IsNullOrEmpty($module)) -and $m -ne $module) {
+            continue
+        }
+        # Copy the manifest
+        Copy-Item "$($modules[$m].srcPath)\$($modules[$m].moduleName).psd1" -Destination $modules[$m].modulePath
+
+        $moduleScriptFiles = Get-ChildItem $modules[$m].srcPath -Filter *.ps1 -File -Recurse
 
         $moduleManifestData = @{
             Path              = "$($modules[$m].modulePath)\$($modules[$m].moduleName).psd1"
@@ -125,12 +142,15 @@ task ModuleBuild Clean, DownloadDependencies, {
             FunctionsToExport = ($moduleScriptFiles | Where-Object { $_.FullName -match "(\\|\/)public(\\|\/)[^\.]+\.ps1$" }).basename
             ModuleVersion     = $version
             <#RequiredModules   = @{
-            ModuleName = 'PSMutagen'
-            RequiredVersion = [version]'0.0.1'
-        }#>
+                ModuleName = 'PSMutagen'
+                RequiredVersion = [version]'0.0.1'
+            }#>
         }
         if ($null -ne $preRelease) {
             $moduleManifestData['Prerelease'] = $preRelease
+        }
+        if (Test-Path "$($modules[$m].modulePath)\$($modules[$m].moduleName).format.ps1xml") {
+            $moduleManifestData['FormatsToProcess'] = "$($modules[$m].moduleName).format.ps1xml"
         }
         Update-ModuleManifest @moduleManifestData
     }
@@ -158,5 +178,22 @@ task Publish Test, DocBuild, {
         }
     }
 }
+
+task QuickClean {
+    foreach ($m in $modules.Keys) {
+        if ((-not [string]::IsNullOrEmpty($module)) -and $m -ne $module) {
+            continue
+        }
+        if (Get-Module $modules[$m].moduleName) {
+            Remove-Module $modules[$m].moduleName
+        }
+        $psm1Path = "$($modules[$m].modulePath)\$($modules[$m].moduleName).psm1"
+        if (Test-Path $psm1Path) {
+            Remove-Item $psm1Path -Force -ErrorAction Ignore | Out-Null
+        }
+    }
+}
+
+task QuickBuild QuickClean, BuildModuleFile, GenerateFormats
 
 task All ModuleBuild, Publish

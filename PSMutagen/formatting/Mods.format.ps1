640b86fc0b6d1c299fff91fb7a5f0d1fa001b561
Anthony Howell <anthony@howell-it.com>
2023-09-03 20:34:04 -0700

    cleaning unneeded formats

--- PSMutagen/formatting/Mods.format.ps1
+++
@@ -1,9 +0,0 @@
-$properties = @('ModKey', 'GameRelease')
-
-# Skyrim
-Write-FormatView -TypeName Mutagen.Bethesda.Skyrim.SkyrimMod -Property $properties -AutoSize
-Write-FormatView -TypeName Mutagen.Bethesda.Skyrim.SkyrimModBinaryOverlay -Property $properties -AutoSize
-
-# Fallout 4
-Write-FormatView -TypeName Mutagen.Bethesda.Fallout4.Fallout4Mod -Property $properties -AutoSize
-Write-FormatView -TypeName Mutagen.Bethesda.Fallout4.Fallout4ModBinaryOverlay -Property $properties -AutoSize

7b0719f0d7ff9a5e7d6ba60be6f45b5cbb1fa96c
Anthony Howell <anthony@howell-it.com>
2023-09-03 20:28:18 -0700

    consolidating formats

--- PSMutagen.Fallout4/formatting/MajorRecords.format.ps1
+++ PSMutagen.Fallout4/formatting/MajorRecords.format.ps1
@@ -1,1 +1,1 @@
-Write-FormatView -TypeName Mutagen.Bethesda.Skyrim.SkyrimMajorRecord -Property @('EditorID', 'TitleString', 'FormKey', 'FormVersion', 'Type') -AutoSize
+Write-FormatView -TypeName Mutagen.Bethesda.Fallout4.Fallout4MajorRecord -Property @('EditorID', 'TitleString', 'FormKey', 'FormVersion', 'Type') -AutoSize
--- PSMutagen.Fallout4/formatting/Mods.format.ps1
+++ PSMutagen.Fallout4/formatting/Mods.format.ps1
@@ -1,9 +1,3 @@
 $properties = @('ModKey', 'GameRelease')
-
-# Skyrim
-Write-FormatView -TypeName Mutagen.Bethesda.Skyrim.SkyrimMod -Property $properties -AutoSize
-Write-FormatView -TypeName Mutagen.Bethesda.Skyrim.SkyrimModBinaryOverlay -Property $properties -AutoSize
-
-# Fallout 4
 Write-FormatView -TypeName Mutagen.Bethesda.Fallout4.Fallout4Mod -Property $properties -AutoSize
 Write-FormatView -TypeName Mutagen.Bethesda.Fallout4.Fallout4ModBinaryOverlay -Property $properties -AutoSize

c5843dc83d503e4d904fc23ec622a99015036865
Anthony Howell <anthony@howell-it.com>
2023-09-05 19:28:23 -0700

    adding skyrim

---
+++ PSMutagen.Skyrim/PSMutagen.Skyrim.cs
@@ -0,0 +1,109 @@
+using System.Management.Automation;
+using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Environments;
+using Mutagen.Bethesda.Plugins.Records;
+using Noggog;
+using System.Reflection.Metadata;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Binary.Parameters;
+using System.IO.Abstractions;
+
+namespace PSMutagen.Skyrim
+{
+    [Cmdlet(VerbsCommon.Get, "SkyrimMod", DefaultParameterSetName = "readwrite")]
+    [OutputType(typeof(ISkyrimModDisposableGetter), ParameterSetName = new string[] { "readonly" })]
+    [OutputType(typeof(ISkyrimMod), ParameterSetName = new string[] { "readwrite" })]
+    public class GetSkyrimMod : Cmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public Mutagen.Bethesda.Plugins.ModPath Path;
+
+        [Parameter()]
+        public Mutagen.Bethesda.Skyrim.GroupMask ImportMask;
+
+        [Parameter()]
+        public Mutagen.Bethesda.Strings.StringsReadParameters StringsParam;
+
+        [Parameter()]
+        public bool Parallel;
+
+        [Parameter()]
+        public System.IO.Abstractions.IFileSystem FileSystem;
+
+        [Parameter(Mandatory = true)]
+        public SkyrimRelease Release;
+
+        [Parameter(ParameterSetName = "readonly")]
+        public SwitchParameter ReadOnly;
+
+        protected override void ProcessRecord()
+        {
+            if (ReadOnly.IsPresent)
+            {
+                WriteObject(SkyrimMod.CreateFromBinaryOverlay(Path, Release, StringsParam, FileSystem));
+            }
+            else
+            {
+                WriteObject(SkyrimMod.CreateFromBinary(Path, Release, ImportMask, StringsParam, Parallel, FileSystem));
+            }
+        }
+    }
+
+    [Cmdlet(VerbsCommon.New, "SkyrimMod")]
+    [OutputType(typeof(ISkyrimMod))]
+    public class NewSkyrimMod : Cmdlet
+    {
+        [Parameter(Mandatory = true)]
+        public ModKey ModKey;
+
+        [Parameter(Mandatory = true)]
+        public SkyrimRelease Release;
+
+        protected override void ProcessRecord()
+        {
+            WriteObject(new SkyrimMod(ModKey, Release));
+        }
+    }
+
+    [Cmdlet(VerbsCommunications.Write, "SkyrimMod")]
+    public class WriteSkyrimMod : Cmdlet
+    {
+        [Parameter()]
+        public IMod Mod;
+
+        [Parameter()]
+        public FileInfo Path;
+
+        [Parameter()]
+        public BinaryWriteParameters BinaryWriteParameters = BinaryWriteParameters.Default;
+
+        [Parameter()]
+        public ParallelWriteParameters ParallelWriteParameters = ParallelWriteParameters.Default;
+
+        [Parameter()]
+        public IFileSystem FileSystem;
+
+        [Parameter()]
+        public SwitchParameter SkipCompressionFix;
+
+        protected override void ProcessRecord()
+        {
+            if (!SkipCompressionFix.IsPresent)
+            {
+                foreach (var rec in Mod.EnumerateMajorRecords())
+                {
+                    rec.IsCompressed = false;
+                }
+            }
+            if (ParallelWriteParameters != null)
+            {
+                Mod.WriteToBinaryParallel(Path, BinaryWriteParameters, FileSystem, ParallelWriteParameters);
+            }
+            else
+            {
+                Mod.WriteToBinary(Path, BinaryWriteParameters, FileSystem);
+            }
+        }
+    }
+}

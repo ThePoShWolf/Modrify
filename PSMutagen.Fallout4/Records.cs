51c13927eed85e8c429e28864895f5a10dbc008d
Anthony Howell <anthony@howell-it.com>
2023-09-07 20:35:45 -0700

    adding modkey

--- PSMutagen.Fallout4/Records.cs
+++ PSMutagen.Fallout4/Records.cs
@@ -5,6 +5,7 @@ using Mutagen.Bethesda.Plugins.Records;
 using Noggog;
 using System.Reflection.Metadata;
 using PSMutagen.Core;
+using Mutagen.Bethesda.Plugins;
 
 namespace PSMutagen.Fallout
 {
@@ -1570,14 +1571,18 @@ namespace PSMutagen.Fallout
         }
     }
 
-    [Cmdlet(VerbsCommon.Get, "FalloutMajorRecords")]
+    [Cmdlet(VerbsCommon.Get, "FalloutMajorRecords", DefaultParameterSetName = "bymodkey")]
     [OutputType(typeof(IEnumerable<IMajorRecordGetter>))]
     public class GetFalloutMajorRecords : PSCmdlet
     {
-        [Parameter(Mandatory = true, ValueFromPipeline = true)]
-        public required IModGetter Mod;
+        [Parameter(Mandatory = true, Position = 0, ValueFromPipeline = true, ParameterSetName = "bymod")]
+        public IFallout4ModGetter? Mod;
 
-        [Parameter()]
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "bymodkey")]
+        public ModKey? ModKey;
+
+        [Parameter(ParameterSetName = "bymodkey")]
+        [Parameter(ParameterSetName = "bymod")]
         // set taken from Helpers
         // "$($mrs.Name -join '","')"
         [ValidateSet("ActionRecord", "Activator", "ActorValueInformation", "ADamageType", "AddonNode", "Ammunition", "APlacedTrap", "Armor", "ArmorAddon", "ArtObject", "AStoryManagerNode", "Book", "CameraPath", "CameraShot", "Cell", "Climate", "CollisionLayer", "ColorRecord", "CombatStyle", "Container", "DialogBranch", "DialogResponses", "DialogTopic", "Door", "EffectShader", "EncounterZone", "EquipType", "Explosion", "Faction", "Furniture", "GameSetting", "Global", "Grass", "Hazard", "HeadPart", "Holotape", "IdleAnimation", "IdleMarker", "ImageSpace", "ImageSpaceAdapter", "Impact", "Ingestible", "Ingredient", "InstanceNamingRules", "Key", "Keyword", "LeveledItem", "LeveledNpc", "Light", "LoadScreen", "Location", "MagicEffect", "MaterialObject", "MaterialSwap", "MaterialType", "Message", "MiscItem", "MovableStatic", "MusicTrack", "MusicType", "NavigationMesh", "Npc", "ObjectEffect", "AObjectModification", "Package", "PackIn", "Perk", "PlacedNpc", "PlacedObject", "Projectile", "Quest", "Race", "Region", "Relationship", "Scene", "ShaderParticleGeometry", "SoundCategory", "SoundDescriptor", "SoundOutputModel", "Static", "StaticCollection", "TalkingActivator", "Terminal", "TextureSet", "Transform", "Tree", "VisualEffect", "VoiceType", "Water", "Weapon", "Weather", "Worldspace", "Zoom", "AttractionRule", "Component", "LocationReferenceType", "AnimationSoundTagSet", "Class", "Debris", "FormList", "ImpactDataSet", "LeveledSpell", "Outfit", "SoundMarker", "AcousticSpace", "ReverbParameters", "LandscapeTexture", "Spell", "Footstep", "FootstepSet", "GodRays", "LensFlare", "Flora", "BodyPartData", "MovementType", "DualCastData", "ConstructibleObject", "AimModel", "BendableSpline", "NavigationMeshInfoMap", "LightingTemplate", "Layer", "ReferenceGroup", "Landscape", "AnimatedObject", "DefaultObjectManager", "DefaultObject", "DialogView", "AssociationType", "AudioEffectChain", "SoundKeywordMapping", "SceneCollection", "AudioCategorySnapshot", "NavigationMeshObstacleManager", "ObjectVisibilityManager")]
@@ -1585,6 +1590,11 @@ namespace PSMutagen.Fallout
 
         protected override void ProcessRecord()
         {
+            if (ParameterSetName == "bymodkey")
+            {
+                ModPath path = $"{PSMutagenConfig.TryGetEnvironment().DataFolderPath}\\{ModKey.ToString()}";
+                Mod = Fallout4Mod.CreateFromBinaryOverlay(path, PSMutagenConfig.TryGetEnvironment().GameRelease.ToSkyrimRelease());
+            }
             if (RecordType == null)
             {
                 WriteObject(Mod.EnumerateMajorRecords());

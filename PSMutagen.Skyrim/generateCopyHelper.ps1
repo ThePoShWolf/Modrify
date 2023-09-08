68ce6bbc147b6584843c9e1d4a7252f9909ed506
Anthony Howell <anthony@howell-it.com>
2023-09-07 19:24:29 -0700

    add return to copy

--- PSMutagen.Skyrim/generateCopyHelper.ps1
+++ PSMutagen.Skyrim/generateCopyHelper.ps1
@@ -1,22 +1,27 @@
+$template = @'
+        public static SkyrimMajorRecord CopyHelper(ISkyrimMod mod, ISkyrimMajorRecordGetter Record, CopyType copyType)
+        {
+            switch (Record.GetType().Name, copyType)
+            {
+{cases}
+                default:
+                    throw new ArgumentException($"Unsupported or improperly implemented type: {Record.GetType().Name}. Please raise an issue in PSMutagen's GitHub repository.");
+            }
+        }
+'@
+
 $switchCaseTemplate = @'
                 case ("{type}", CopyType.AsOverride):
                 case ("{type}BinaryOverlay", CopyType.AsOverride):
-                    mod.{name}.GetOrAddAsOverride(Record);
-                    break;
+                    return mod.{name}.GetOrAddAsOverride(Record);
                 case ("{type}", CopyType.AsNewRecord):
                 case ("{type}BinaryOverlay", CopyType.AsNewRecord):
-                    mod.{name}.DuplicateInAsNewRecord(Record);
-                    break;
+                    return mod.{name}.DuplicateInAsNewRecord(Record);
                 case ("{type}", CopyType.DeepCopy):
                 case ("{type}BinaryOverlay", CopyType.DeepCopy):
                     {type} new{type}Record = ({type})Record.DeepCopy();
                     mod.{name}.Add(new{type}Record);
-                    break;
-'@
-
-$default = @'
-                default:
-                    throw new ArgumentException($"Unsupported or improperly implemented type: {Record.GetType().Name}. Please raise an issue in PSMutagen's GitHub repository.");
+                    return new{type}Record;
 '@
 
 #Import-Module ".\build\PSMutagen\"
@@ -26,7 +31,7 @@ $ge = Set-MutaGameEnvironment -Game SkyrimSE -PassThru
 $props = $ge.LoadOrder.PriorityOrder.ToArray()[-1].Mod.PSObject.Properties | Where-Object { $_.MemberType -eq 'Property' -and $_.TypeNameOfValue -like 'Mutagen.Bethesda.Skyrim.ISkyrimGroupGetter*' }
 $regex = '^Mutagen\.Bethesda\.Skyrim\.ISkyrimGroupGetter\`1\[\[Mutagen\.Bethesda\.Skyrim\.I(?<type>[^,]+)Getter'
 
-$switches = foreach ($prop in $props) {
+$cases = foreach ($prop in $props) {
     if ($prop.TypeNameOfValue -match $regex) {
         $switchCaseTemplate -replace '\{type\}', $Matches.type -replace '\{name\}', $prop.Name
     } else {
@@ -34,4 +39,4 @@ $switches = foreach ($prop in $props) {
     }
 }
 
-@($switches + $default) -join "`n" | clip
+$template -replace '\{cases\}', ($cases + $default -join "`n") | clip
